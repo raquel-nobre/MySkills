bd4fd6f35228e365526f4e5c48fab81180163cde
Raquel Nobre <raquelsouzanobre@gmail.com>
2022-06-11 09:36:41 -0300

    firts commit

---
+++ src/pages/Home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+     View,
+     Text, 
+     StyleSheet, 
+     TextInput, 
+     Platform,
+     TouchableOpacity
+    } from 'react-native';
+
+export function Home(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>
+          Welcome, Raquel
+      </Text>
+
+      <TextInput 
+           style={styles.Input}
+           placeholder="New Skill"
+           placeholderTextColor="#555"
+      />
+
+      <TouchableOpacity 
+          style={styles.button}
+          activeOpacity={0.7}
+          >
+          <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, {marginTop: 50 }]}>
+          My Skills 
+      </Text>
+
+    </View>
+  
+  )
+}
+
+const styles = StyleSheet.create({
+   container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+
+   },
+   title: {
+       color: '#fff',
+       fontSize: 24,
+       fontWeight: 'bold'
+   },
+   Input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+   },
+   button: {
+       backgroundColor: '#A370F7',
+       padding: 15,
+       borderRadius: 7,
+       alignItems: 'center',
+       marginTop: 20,
+   },
+   buttonText: {
+       color: '#FFF',
+       fontSize: 17,
+       fontWeight: 'bold'
+   }
+});
